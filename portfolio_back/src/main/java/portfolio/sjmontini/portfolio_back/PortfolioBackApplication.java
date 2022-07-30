0f08f9b45bf2655a7c6480895b49b4acd7c5572a
Sebastian Montini <54865232+sebasmontini@users.noreply.github.com>
2022-07-30 20:20:43 -0300

    Arranca Back2

--- portfolio_back/src/main/java/portfolio/sjmontini/portfolio_back/PortfolioBackApplication.java
+++ portfolio_back/src/main/java/portfolio/sjmontini/portfolio_back/PortfolioBackApplication.java
@@ -1,4 +1,4 @@
-package portfolio_back;
+package portfolio.sjmontini.portfolio_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- portfolio_back/src/test/java/portfolio/sjmontini/portfolio_back/PortfolioBackApplicationTests.java
+++ portfolio_back/src/test/java/portfolio/sjmontini/portfolio_back/PortfolioBackApplicationTests.java
@@ -1,4 +1,4 @@
-package portfolio_back;
+package portfolio.sjmontini.portfolio_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
