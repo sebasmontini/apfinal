e92ee29f9e6abca39a1a21eb75a75ae27cfc32fd
Sebastian Montini <54865232+sebasmontini@users.noreply.github.com>
2022-07-31 02:43:20 -0300

    Back 1

--- portfolio_back/src/main/java/portfolio/sjmontini/portfolio_back/model/Person.java
+++ portfolio_back/src/main/java/portfolio/sjmontini/portfolio_back/model/Person.java
@@ -6,17 +6,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
 @Entity
 public class Person implements Serializable{
     @Id
