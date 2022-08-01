b5d55f46c1f62d08f748f80eeb268ff41463f610
Sebastian Montini <54865232+sebasmontini@users.noreply.github.com>
2022-07-31 22:02:44 -0300

    Cambios de Repos
    Hasta acá fue este repo. Luego se separaron front y back

--- portfolio_back/src/main/java/portfolio/sjmontini/portfolio_back/model/Person.java
+++ portfolio_back/src/main/java/portfolio/sjmontini/portfolio_back/model/Person.java
@@ -6,17 +6,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
 public class Person implements Serializable{
     @Id
@@ -28,6 +28,11 @@ public class Person implements Serializable{
     private String email;
     private String phone;
     private String ocupation;
-    private String imageUrl;
-    private String aboutMe;
+    private String imageurl;
+    private String aboutme;
+    private String linkedinurl;
+    private String instagramurl;
+    private String facebookurl;
+    private String twitterurl;
+    private String githuburl;
 }
